dd747fdf8ee46533cdfc8089d214e5977c4e9982
abhishek18620 <abhishek18620@gmail.com>
2018-01-02 11:13:49 +0530

    Included my ML practise section here too

---
+++ src/__main__.py
@@ -0,0 +1,14 @@
+# Let's start multitasking with some web scraping
+# I'll scrape through my router page and grab active clients with all  information possible
+import sys
+import argparse
+
+# parser=argparse.ArgumentParser(desciption="Hello World")
+# parser.add_argument("-p","--print",)
+def ():
+
+
+
+if __name__=='__main__':
+    main()
+
