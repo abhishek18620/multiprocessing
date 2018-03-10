61dc9ddfb1e126510deed536021f395c3e8a8abb
abhishek18620 <abhishek18620@gmail.com>
2018-03-11 01:38:53 +0530

    Uploading "Routing in a Delay Tolerant Network"

---
+++ src/ASYNCIO.py
@@ -0,0 +1,17 @@
+"""
+Author : abhishek18620
+Date : 2018-02-09
+File : ASYNCIO
+
+Topic : Asyncio
+Link : http://www.aosabook.org/en/500L/a-web-crawler-with-asyncio-coroutines.html
+"""
+class sample():
+    def __init__(self):
+        self.value=1
+        self.s="Hello"
+    def __repr__(self):
+        return "value : "+repr(self.value)
+
+obj=sample()
+print(obj)
