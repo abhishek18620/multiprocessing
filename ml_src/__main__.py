f8279975ccc93c209421d4500efd162d3f0130f4
abhishek18620 <abhishek18620@gmail.com>
2018-03-26 17:11:25 +0530

    Trying out asyncio

--- ml_src/__main__.py
+++
@@ -1,3 +0,0 @@
-# These scripts are for machine learning course
-# link : https://www.youtube.com/playlist?list=PLA89DCFA6ADACE599
-
--- src/aio_stopinput.py
+++ src/aio_stopinput.py
@@ -6,6 +6,7 @@ File : ASYNCIO
 Topic : Asyncio
 Link : http://www.aosabook.org/en/500L/a-web-crawler-with-asyncio-coroutines.html
 """
+import sys
 import asyncio
 
 def ii():
@@ -33,10 +34,18 @@ def solve2():
         """
         yield from asyncio.sleep(1)
 
+def wait_input(loop, tasks):
+    print("Stopping")
+    for task in tasks:
+        task.cancel()
+    loop.stop()
+
 if __name__=="__main__":
     loop = asyncio.get_event_loop()
     sol1=asyncio.async(solve1())
     sol2=asyncio.async(solve2())
+    print("Hit any key to stop ...")
+    loop.add_reader(sys.stdin, wait_input, loop, [sol1,sol2])
     loop.run_forever()
     loop.close()
     print("Finished")
